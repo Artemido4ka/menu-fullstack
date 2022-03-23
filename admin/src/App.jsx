d8514ee0ba7b7b593ccae2997e13fbd6a2cea2b5
Artsiom Karenka <korenko.artem9@gmail.com>
2022-03-23 13:35:06 +0300

    feat:add admin panel

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+// import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      {/* <Route path="/login" element={<Login />} /> */}
+    </Routes>
+  );
+};
+
+export default App;
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import { Avatar, Badge } from "@material-ui/core";
+import { ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+// import { signout } from "../redux/apiCalls";
+// import { mobile } from "../responsive";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: teal;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+  const signoutHandler = () => {
+    // dispatch(signout());
+    navigate("/");
+  };
+  return (
+    <Container>
+      <Wrapper>
+        <Link to="/">
+          <Left>
+            <Logo>Menu.</Logo>
+          </Left>
+        </Link>
+        <Right>
+          <Link to="/orders">
+            <MenuItem>ORDERS</MenuItem>
+          </Link>
+
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
