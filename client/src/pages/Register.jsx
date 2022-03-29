e10e66a26bffbfeeb6e1507737c4d74a1b17f20b
Artsiom Karenka <korenko.artem9@gmail.com>
2022-03-29 16:39:48 +0300

    feat:change registration page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
-import { registrate } from "../redux/apiCalls";
 import { useNavigate } from "react-router-dom";
+
+import { registrate } from "../redux/apiCalls";
+import { devices } from "../devices";
+
+import TextField from "@material-ui/core/TextField";
 import styled from "styled-components";
-// import { mobile } from "../responsive";
 
 const Container = styled.div`
   width: 100vw;
@@ -21,9 +26,15 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-  width: 40%;
+  width: 75%;
   padding: 20px;
   background-color: white;
+  @media ${devices.tablet} {
+    width: 40%;
+  }
+  @media ${devices.laptopL} {
+    width: 30%;
+  }
 `;
 
 const Title = styled.h1`
@@ -36,37 +47,44 @@ const Form = styled.form`
   flex-wrap: wrap;
 `;
 
-const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
-  margin: 20px 10px 0px 0px;
-  padding: 10px;
-`;
-
-const Agreement = styled.span`
-  font-size: 12px;
-  margin: 20px 0px;
-`;
-
 const Button = styled.button`
+  text-transform: uppercase;
+  margin-top: 20px;
   width: 40%;
   border: none;
   padding: 15px 20px;
   background-color: teal;
   color: white;
   cursor: pointer;
+  margin-bottom: 10px;
+  &:disabled {
+    color: aqua;
+    cursor: not-allowed;
+  }
 `;
 
 const Error = styled.span`
   color: red;
 `;
 
-const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [firstName, setName] = useState("");
+const validationSchema = yup.object({
+  firstName: yup.string("Enter your name").required("Name is required"),
+  lastName: yup.string("Enter your SurName").required("SurName is required"),
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string("Enter your password")
+    .min(4, "Password should be of minimum 4 characters length")
+    .required("Password is required"),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Password Confirmation is required"),
+});
 
+const Register = () => {
   const dispatch = useDispatch();
   const { isFetching, error, user } = useSelector((state) => state.user);
   let navigate = useNavigate();
@@ -77,37 +95,94 @@ const Register = () => {
     }
   }, [navigate, user]);
 
-  const handleRegistrationClick = (e) => {
-    e.preventDefault();
-    registrate(dispatch, { firstName, lastName, password, email });
-  };
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      registrate(dispatch, values);
+    },
+  });
+
   return (
     <Container>
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
-        <Form>
-          <Input placeholder="name" onChange={(e) => setName(e.target.value)} />
-          <Input
-            placeholder="last name"
-            onChange={(e) => setLastName(e.target.value)}
+        <Form onSubmit={formik.handleSubmit}>
+          <TextField
+            fullWidth
+            id="name"
+            name="firstName"
+            label="name"
+            type="text"
+            value={formik.values.firstName}
+            onChange={formik.handleChange}
+            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+            helperText={formik.touched.firstName && formik.errors.firstName}
           />
-          <Input
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
+          <TextField
+            fullWidth
+            id="lastName"
+            name="lastName"
+            label="lastName"
+            type="text"
+            value={formik.values.lastName}
+            onChange={formik.handleChange}
+            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+            helperText={formik.touched.lastName && formik.errors.lastName}
           />
-          <Input placeholder="confirm password" />
-          <Input
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
+
+          <TextField
+            fullWidth
+            id="email"
+            name="email"
+            label="email"
+            type="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
           />
-          <Agreement>
-            By creating an account, I consent to the processing of my personal
-            data in accordance with the <b>PRIVACY POLICY</b>
-          </Agreement>
-          <Button onClick={handleRegistrationClick} disabled={isFetching}>
-            CREATE
+          <TextField
+            fullWidth
+            id="password"
+            name="password"
+            label="password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+          <TextField
+            fullWidth
+            id="passwordConfirmation"
+            name="passwordConfirmation"
+            label="passwordConfirmation"
+            type="password"
+            value={formik.values.passwordConfirmation}
+            onChange={formik.handleChange}
+            error={
+              formik.touched.passwordConfirmation &&
+              Boolean(formik.errors.passwordConfirmation)
+            }
+            helperText={
+              formik.touched.passwordConfirmation &&
+              formik.errors.passwordConfirmation
+            }
+          />
+
+          <Button color="primary" variant="contained" fullWidth type="submit">
+            Submit
           </Button>
           {error && <Error>Something is wrong...</Error>}
+          {/* <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link> */}
+          {/* <Link>CREATE A NEW ACCOUNT</Link> */}
         </Form>
       </Wrapper>
     </Container>
