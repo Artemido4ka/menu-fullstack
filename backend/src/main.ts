c4dd598f5a7668f4344ee35c66cda63312e31142
Artsiom Karenka <korenko.artem9@gmail.com>
2022-04-05 11:56:24 +0300

    feat:config cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,9 @@ dotenv.config();
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
