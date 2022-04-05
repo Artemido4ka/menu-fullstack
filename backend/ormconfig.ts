c1f90a1c79985269317985d15da5653d8dc3dc29
Artsiom Karenka <korenko.artem9@gmail.com>
2022-04-05 14:29:14 +0300

    feat:add config

---
+++ backend/ormconfig.ts
@@ -0,0 +1,28 @@
+import * as dotenv from 'dotenv';
+
+import { Message } from './src/messages/entities/message.entity';
+import { Image } from 'src/images/images.entity';
+import { Order } from './src/orders/orders.entity';
+import { Product } from './src/products/products.entity';
+import { Role } from './src/roles/roles.entity';
+import { User } from './src/users/users.entity';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+dotenv.config();
+
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [User, Role, Product, Order, Image, Message],
+  synchronize: false,
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+};
+
+export default config;
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,45 +1,28 @@
-import { Order } from './orders/orders.entity';
-import { Role } from './roles/roles.entity';
-import { User } from './users/users.entity';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
+
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
 import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
-import { Product } from './products/products.entity';
 import { OrdersModule } from './orders/orders.module';
 import { FilesModule } from './files/files.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import * as path from 'path';
 import { ImagesModule } from './images/images.module';
-import { Image } from './images/images.entity';
 import { MessagesModule } from './messages/messages.module';
-import { Message } from './messages/entities/message.entity';
+import config from 'ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
     }),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static'),
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [User, Role, Product, Order, Image, Message],
-      synchronize: true,
-      migrations: ['dist/src/db/migrations/*.js'],
-      cli: {
-        migrationsDir: 'src/db/migrations',
-      },
-    }),
+    TypeOrmModule.forRoot(config),
     UsersModule,
     RolesModule,
     AuthModule,
