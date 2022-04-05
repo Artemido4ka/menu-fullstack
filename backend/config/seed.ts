6723ad759fa0cdb001b4ce548b12e0a5f4c257ce
Artsiom Karenka <korenko.artem9@gmail.com>
2022-04-05 15:33:52 +0300

    feat:add seed

--- backend/config/seed.ts
+++ backend/config/seed.ts
@@ -10,7 +10,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 dotenv.config();
 
-const config: TypeOrmModuleOptions = {
+const seedConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
@@ -19,10 +19,10 @@ const config: TypeOrmModuleOptions = {
   database: process.env.POSTGRES_DB,
   entities: ['dist/src/**/*.entity{.js,.ts}'],
   synchronize: false,
-  migrations: ['dist/src/db/migrations/*.js'],
+  migrations: ['dist/src/db/seeders/*.js'],
   cli: {
-    migrationsDir: 'src/db/migrations',
+    migrationsDir: 'src/db/seeders',
   },
 };
 
-export default config;
+export default seedConfig;
---
+++ backend/src/db/seeders/1649160347231-Create.ts
@@ -0,0 +1,39 @@
+import { Role } from './../../roles/roles.entity';
+import { getRepository, MigrationInterface } from 'typeorm';
+
+const roles = [
+  {
+    value: 'USER',
+    description: 'administrator',
+  },
+  {
+    value: 'ADMIN',
+    description: 'user',
+  },
+];
+
+export class Create1649160347231 implements MigrationInterface {
+  name = 'Create1649160347231';
+
+  public async up(): Promise<void> {
+    const repository = getRepository(Role);
+    const createRolesPromises = roles.map(async (role) => {
+      return repository.save(role);
+    });
+
+    await Promise.all(createRolesPromises);
+  }
+
+  public async down(): Promise<void> {
+    const repository = getRepository(Role);
+    const deletedRoles = await Promise.all(
+      roles.map(async (role) => {
+        return repository.findOne({
+          value: role.value,
+        });
+      }),
+    );
+
+    await repository.remove(deletedRoles);
+  }
+}
